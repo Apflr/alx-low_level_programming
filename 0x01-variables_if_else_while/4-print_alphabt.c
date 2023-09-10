5780e3f4be4b6bc7db2214d8c3dec481a696b07b
Wacu <springnaomi6@gmail.com>
2023-09-10 11:06:14 -0700

    letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Prints out the alphabet without q and e.
+ * Return: end of program
+ */
+int main(void)
+{
+	int i;
+	for (i = 97; i < 123; i++)
+
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
