f898c004f2fb5251ab9033420ef564a0fd0066f0
Wacu <springnaomi6@gmail.com>
2023-09-27 19:19:29 -0700

    hi

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - function test
+ * @s: pointer to a string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == !'\0')
+	{
+		putchar(*s);
+		putchar('\n');
+	}
+}
