3f24301afd60f81abdf325d3c6cb7e0ef29de55a
Wacu <springnaomi6@gmail.com>
2023-10-04 19:52:45 -0700

    grit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - program execution
+ * main - print the number of arguments passed to the program
  * @argc: number of arguments
- * @argv: aray of strings
- * Return: Always succes
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
 (void) argv; /*Ignore argv*/
