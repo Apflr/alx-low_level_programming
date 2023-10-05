7d04972d30e3b1b0acf5e14bcacae71b726349c9
Wacu <springnaomi6@gmail.com>
2023-10-04 19:49:01 -0700

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program execution
+ * @argc: number of arguments
+ * @argv: aray of strings
+ * Return: Always succes
+ */
+int main(int argc, char *argv[])
+{
+(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+i
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,6 @@
+0x0A. C - argc, argv
+C
+ By: Julien Barbier
+ Weight: 1
+ Ongoing second chance project - started Sep 29, 2023 6:00 AM, must end by Oct 5, 2023 6:00 AM
+ An auto review will be launched at the deadline
