986198140689f0960fd3aee2ea8db0e204527ea0
Wacu <springnaomi6@gmail.com>
2023-10-10 15:02:56 -0700

    new commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,33 +1,19 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include "dog.h"
-#incde <string.h>
+	#include "dog.h"
 
-/**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
- * @name: name to initialize
- * @age: age to initialize
- * @owner: owner to initialize
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-    if (d == NULL)
-    {
-        printf("Memory allocation failed\n");
-        return;
-    }
-    d->name = malloc((strlen(name) + 1) * sizeof(char));
-    d->owner = malloc((strlen(owner) + 1) * sizeof(char));
 
-    if (d->name == NULL || d->owner == NULL)
-    {
-        printf("Memory allocation failed\n");
-        free(d->name);
-        free(d->owner);
-        return;
-    }
-    strcpy(d->name, name);
-    d->age = age;
-    strcpy(d->owner, owner);
-}
+	/**
+	 * init_dog - initializes a variable of type struct dog
+	 * @d: pointer to struct dog to initialize
+	 * @name: name to initialize
+	 * @age: age to initialize
+	 * @owner: owner to initialize
+	 */
+	void init_dog(struct dog *d, char *name, float age, char *owner)
+	{
+		if (d == NULL)
+			d = malloc(sizeof(struct dog));
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
