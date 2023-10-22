c2650c10cdda5e83a445f007b92e2e8745f20b3c
Naomi Duncan <springnaomi@gmail.com>
2023-10-22 05:02:40 -0700

    hi

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,19 @@
+#include "main.h"
 /**
- * _strchr - Locates a character in a string
- * @s: The string to search in
- * @c: The character to locate
- *
- * Return: A pointer to the first occurrence of the character 'c' in the string 's',
- *         or NULL if the character is not found.
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return s;
-		s++;
-	}
+        int i = 0;
 
-	if (*s == c)
-		return s;
-
-	return NULL;
+        for (; s[i] >= '\0'; i++)
+        {
+                if (s[i] == c)
+                        return (&s[i]);
+        }
+        return (0);
 }
 
